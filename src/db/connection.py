4c537fe00b0a4438ca95edaccf2f0c992942e370
Akylai Kubanychbekova <akilaykubanichbekova2016@gmail.com>
2026-01-20 14:42:33 +0600

    Безопасное подключение к БД, удалён test_connection.py

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,14 +1,37 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 import psycopg2
 
-load_dotenv()
+# Абсолютный путь к .env
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+dotenv_path = BASE_DIR / ".env"
+load_dotenv(dotenv_path)
+
+# Берём переменные из .env
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+
+print("DB_HOST:", DB_HOST)
+print("DB_USER:", DB_USER)
+print("DB_PASSWORD:", "есть" if DB_PASSWORD else "пусто")
 
 def get_db_connection():
-    return psycopg2.connect(
-        host=os.getenv("POSTGRES_HOST"),
-        port=os.getenv("POSTGRES_PORT"),
-        dbname=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD")
+    if None in (DB_HOST, DB_PORT, DB_NAME, DB_USER):
+        raise ValueError("Одна из переменных окружения не задана!")
+
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
     )
+    return conn
+
+
+
+
